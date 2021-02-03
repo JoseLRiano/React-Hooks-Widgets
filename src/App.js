2d00eb445be27d051d63f2dd13f570443b98ded3
JoseLRiano <joselriano@yahoo.com>
2021-02-02 19:33:10 -0500

    Dummy Data added as an array of Objects and semantic ui added as well for styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Accordion from './components/Accordion';
-
-export default () => {
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why is React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creatign components'
+    }
+];
+const App = () => {
     return (
         <div>
-            <Accordion />
+        <br />
+            <Accordion items={items} />
         </div>
     );
 };
+export default App;
